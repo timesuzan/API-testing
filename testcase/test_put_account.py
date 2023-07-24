282ea20df09adf3e3e92d1deedf626bbb6bf8a99
Inianft <1138413378@qq.com>
2023-07-24 18:01:25 +0800

    fix put account case

--- testcase/test_put_account.py
+++ testcase/test_put_account.py
@@ -1,15 +1,17 @@
 import pytest
 from commons.yaml_util import *
 from commons.request_util import RequestUtil
+import config
 import jsonpath
+
 
 class TestChangeAccount:
 
     @pytest.mark.run(order=3)
     @pytest.mark.parametrize("findaccount", read_yaml_testcase("config/test_put_account.yaml"))
-    def test_change_account(self,findaccount):
+    def test_change_account(self, findaccount):
         method = findaccount["request"]["method"]
-        url = findaccount["request"]["url"]
+        url = findaccount["request"]["url"] + '/' + str(read_yaml().get("account_id"))
         headers = findaccount["request"]["Authentication"]
         data = findaccount["request"]["body"]
         res = RequestUtil().send_all_request(method=method, url=url, headers=headers, data=data)
@@ -18,24 +20,24 @@ class TestChangeAccount:
     @pytest.mark.parametrize("findaccount", read_yaml_testcase("config/test_put_account.yaml"))
     def test_no_authorized_account(self, findaccount):
         method = findaccount["request"]["method"]
-        url = findaccount["request"]["url"]
+        url = findaccount["request"]["url"] + '/' + str(read_yaml().get("account_id"))
         data = findaccount["request"]["body"]
         res = RequestUtil().send_all_request(method=method, url=url, data=data)
         assert res.status_code == 401
 
     @pytest.mark.parametrize("findaccount", read_yaml_testcase("config/test_put_account.yaml"))
-    def test_change_no_id_account(self,findaccount):
+    def test_change_no_id_account(self, findaccount):
         method = findaccount["request"]["method"]
         url = "https://3x5m5o-3000.csb.app/customer/600"
         data = findaccount["request"]["body"]
         headers = findaccount["request"]["Authentication"]
-        res = RequestUtil().send_all_request(method=method, url=url,  data=data, headers=headers)
+        res = RequestUtil().send_all_request(method=method, url=url, data=data, headers=headers)
         assert res.status_code == 400
 
     @pytest.mark.parametrize("findaccount", read_yaml_testcase("config/test_put_account.yaml"))
     def test_change_no_data_account(self, findaccount):
         method = findaccount["request"]["method"]
-        url = findaccount["request"]["url"]
+        url = findaccount["request"]["url"] + '/' + str(read_yaml().get("account_id"))
         headers = findaccount["request"]["Authentication"]
-        res = RequestUtil().send_all_request(method=method, url=url,  headers=headers)
+        res = RequestUtil().send_all_request(method=method, url=url, headers=headers)
         assert res.status_code == 400
